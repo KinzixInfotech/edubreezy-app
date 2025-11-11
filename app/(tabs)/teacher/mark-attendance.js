4ffe97f063355db37c7d979a0be23b9472acbac0
Mansha Jami <manshajami@Manshas-MacBook-Pro.local>
2025-11-12 00:51:50 +0530

    updated

---
+++ app/(tabs)/teacher/mark-attendance.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const markattendance = () => {
+  return (
+    <View>
+      <Text>mark-attendance</Text>
+    </View>
+  )
+}
+
+export default mark-attendance
+
+const styles = StyleSheet.create({})

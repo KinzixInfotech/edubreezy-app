d09af06c55dea141d4653fedc39f615c98164e56
gigxmanshajami <mansajami2020@gmail.com>
2026-04-16 01:48:39 +0530

    updated

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
         name: "EduBreezy",
         slug: "EduBreezy",
         scheme: "edubreezy",
-        version: "1.0.5",
+        version: "1.0.6",
         icon: "./assets/icon.png",
         userInterfaceStyle: "light",
         newArchEnabled: true,
